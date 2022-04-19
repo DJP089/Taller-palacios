4c1dae08f177627e6d056cf0705329873a5c312c
DJP089 <97998759+DJP089@users.noreply.github.com>
2022-04-19 12:50:29 -0600

    2commit

--- database/get_estados.py
+++ database/get_estados.py
@@ -6,7 +6,8 @@ from os.path import relpath
 import os
 def get_estados_from_file():
     current_dir = os.getcwd()
-    data_frame = pd.read_csv( relpath('./estados.csv'), sep=";")
+    print(current_dir)
+    data_frame = pd.read_csv( relpath('./database/estados.csv'), sep=";")
     for desc, _ in data_frame.values:
         estado = Estados(desc)
         db.session.add(estado)
--- database/get_tipos.py
+++ database/get_tipos.py
@@ -5,7 +5,8 @@ from os.path import relpath
 import os
 def get_tipos_from_file():
     current_dir = os.getcwd()
-    data_frame = pd.read_csv( relpath('./tipos.csv'), sep=";")
+    
+    data_frame = pd.read_csv( relpath('./database/tipos.csv'), sep=";")
     for desc, _ in data_frame.values:
         tipo = Tipos(desc)
         db.session.add(tipo)
